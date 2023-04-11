62e77aa2c1433f3fc7a566705e152f2bdf1c7b1e
sergd <videomc1979@gmail.com>
2023-04-11 21:58:05 +0300

    Начало курсовой

--- users/models.py
+++ users/models.py
@@ -5,14 +5,12 @@ from phonenumber_field.modelfields import PhoneNumberField
 from django.utils.translation import gettext_lazy as _
 
 
-# Create your models here.
-class UserRoles(models.Model):
+class UserRoles:
     # TODO закончите enum-класс для пользователя
-    name_user = models.CharField(max_length=100)
-    phone = PhoneNumberField(null=False, blank=False, unique=True)
+    pass
 
 
-# class User(AbstractBaseUser):
-#     # TODO переопределение пользователя.
-#     # TODO подробности также можно поискать в рекоммендациях к проекту
-#     pass
+class User(AbstractBaseUser):
+    # TODO переопределение пользователя.
+    # TODO подробности также можно поискать в рекоммендациях к проекту
+    pass
