2588d5ce1b2ba534c0ffed763721ec1f26a6c6bc
sergd <videomc1979@gmail.com>
2023-04-13 15:20:52 +0300

    CW_6 подготовка ads

--- ads/urls.py
+++ ads/urls.py
@@ -1,8 +1,12 @@
 from django.urls import include, path
+from rest_framework.routers import SimpleRouter
+
+from ads.views import AdViewSet
 
 # TODO настройка роутов для модели
-
+ad_router = SimpleRouter()
+ad_router.register("ads", AdViewSet, basename="ads")
 
 urlpatterns = [
-
+    path("", include(ad_router.urls)),
 ]
--- ads/views.py
+++ ads/views.py
@@ -1,13 +1,18 @@
 from rest_framework import pagination, viewsets
 
+from ads.models import Ad
+from ads.serializers import AdSerializer
+
 
 class AdPagination(pagination.PageNumberPagination):
-    pass
+    page_size = 4
 
 
 # TODO view функции. Предлагаем Вам следующую структуру - но Вы всегда можете использовать свою
 class AdViewSet(viewsets.ModelViewSet):
-    pass
+    queryset = Ad.objects.all()
+    serializer_class = AdSerializer
+    pagination_class = AdPagination
 
 
 class CommentViewSet(viewsets.ModelViewSet):
