50396cd10f757cff2dd2109f91e66c74422c8378
sergd <videomc1979@gmail.com>
2023-04-13 15:20:10 +0300

    CW_6 правка urls

--- skymarket/urls.py
+++ skymarket/urls.py
@@ -29,6 +29,8 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
 
+    path('', include('ads.urls')),
+
     # === API Document ===
     # YOUR PATTERNS
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
