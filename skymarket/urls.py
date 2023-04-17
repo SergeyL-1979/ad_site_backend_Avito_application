8375f1d91495b1c07ea00493b99baccb1d2bd6e8
sergd <videomc1979@gmail.com>
2023-04-17 15:44:44 +0300

    CW_6 authtoken

--- skymarket/urls.py
+++ skymarket/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
 
     path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
     path('auth/', include('djoser.urls.jwt')),
 
     path('', include('ads.urls')),
