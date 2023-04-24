ea328467202c16dd7c5e2a833e1d88782338ae36
sergd <videomc1979@gmail.com>
2023-04-24 17:14:44 +0300

    CW_6 permissions ready

--- ads/permissions.py
+++ ads/permissions.py
@@ -1,7 +1,6 @@
 # здесь производится настройка пермишенов для нашего проекта
-
-
 from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
 from users.models import UserRoles
 
@@ -19,3 +18,11 @@ class IsExecutor(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return obj.author == request.user
+
+
+# class IsOwnerOrReadOnly(BasePermission):
+#     def has_object_permission(self, request, view, obj):
+#         if request.method in permissions.SAFE_METHODS:
+#             return True
+#
+#         return obj.author == request.user
